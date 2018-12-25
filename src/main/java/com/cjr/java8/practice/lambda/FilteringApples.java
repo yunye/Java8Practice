5cef8e534fe5df210b3e366e97bb5c3b543f71ed
chengjiarong1 <chengjiarong1@crc.com.hk>
2018-12-25 19:09:31 +0800

    实践并理解java8的 "行为参数化"思想

--- src/main/java/com/cjr/java8/practice/lambda/FilteringApples.java
+++ src/main/java/com/cjr/java8/practice/lambda/FilteringApples.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
+/**
+ * 集合过滤示例: 实践 java 8 的 "行为参数化"思想， lambda的类型推断
+ * @author cjr
+ * @date 2018-12-25
+ *
+ */
 public class FilteringApples {
 	
 	public static void main(String ...args) {
@@ -12,10 +18,25 @@ public class FilteringApples {
 		inventory.add(new Apple(200, "green"));
 		inventory.add(new Apple(300, "red"));
 		
+		//1. 过滤绿色的苹果： 传统做法，写一个方法循环遍历并判断
 		List<Apple> greenApples = filterGreenApples(inventory);
 		System.out.println(greenApples);
 		
-		greenApples = filterApples(inventory, (Apple t) -> "red".equals(t.getColor()));
+		//2. 过滤绿色的苹果： 传统做法，使用匿名内部内传递过滤行为
+		greenApples = filterGreenApples(inventory, new IFilterApple() {
+			@Override
+			public boolean test(Apple apple) {
+				return "green".equals(apple.getColor());
+			}
+		});
+		System.out.println(greenApples);
+		
+		//3. 过滤绿色的苹果： 使用lambda表达式
+		greenApples = filterApples(inventory, (Apple t) -> "green".equals(t.getColor()));
+		System.out.println(greenApples);
+
+		//3. 过滤绿色的苹果： 使用lambda表达式
+		greenApples = filterApples(inventory, t -> "green".equals(t.getColor()));
 		System.out.println(greenApples);
 		
 	}
@@ -24,6 +45,16 @@ public class FilteringApples {
 		List<Apple> result = new ArrayList<>();
 		for(Apple apple: inventory) {
 			if("green".equals(apple.getColor())) {
+				result.add(apple);
+			}
+		}
+		return result;
+	}
+	
+	public static List<Apple> filterGreenApples(List<Apple> inventory, IFilterApple iFilterApple){
+		List<Apple> result = new ArrayList<>();
+		for(Apple apple: inventory) {
+			if(iFilterApple.test(apple)) {
 				result.add(apple);
 			}
 		}
