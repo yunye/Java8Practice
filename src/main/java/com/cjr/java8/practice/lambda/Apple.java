84fee5aa671c9cdaf69109bc67c3bc7403cee0ee
chengjiarong1 <chengjiarong1@crc.com.hk>
2018-12-27 21:13:14 +0800

    eclipse设置使用4个空格代替tab

--- src/main/java/com/cjr/java8/practice/lambda/Apple.java
+++ src/main/java/com/cjr/java8/practice/lambda/Apple.java
@@ -2,42 +2,41 @@ package com.cjr.java8.practice.lambda;
 
 /**
  * 苹果
+ * 
  * @author cjr
  * @date 2018-12-24
  *
  */
 public class Apple {
-	/** 重量. */
-	private int weight = 0;
+    /** 重量. */
+    private int weight = 0;
 
-	/** 颜色. */
-	private String color = "";
-	
-	public Apple(int weight, String color) {
-		this.weight = weight;
-		this.color = color;
-	}
+    /** 颜色. */
+    private String color = "";
 
-	public int getWeight() {
-		return weight;
-	}
+    public Apple(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
 
-	public void setWeight(int weight) {
-		this.weight = weight;
-	}
+    public int getWeight() {
+        return weight;
+    }
 
-	public String getColor() {
-		return color;
-	}
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 
-	public void setColor(String color) {
-		this.color = color;
-	}
-	
-	public String toString() {
-		return "Apple{" + 
-				"color='" + color + '\'' + 
-				", weight=" + weight + 
-				'}'; 
-	}
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Apple{" + "color='" + color + '\'' + ", weight=" + weight + '}';
+    }
 }
